1cc78f6040ced23bf5185bc3f1dd27818470ae13
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-10-05 20:48:25 +0200

    added age verification and discounts

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,49 @@
-//Price per km
-
+// Price per km
 const kmPrice = 0.21;
 
-//Distance request
+// Distance request
+let distance = prompt('Inserisci i km che vuoi percorrere');
 
-let distance = prompt('inserisci i km che vuoi percorrere');
-
-//Distance conversion into number
-
+// Distance conversion into number
 let km = Number(distance);
 
-//Verification if prompt input is a number or not
+//Asigned viriables
 
 let price;
+let priceUnder18;
+let priceOver65;
+const discount20 = 0.2;
+const discount40 = 0.4;
 
-if (!isNaN(km)){
+// Verification if prompt input is a number or not
+
+if (!isNaN(km)) {
     price = km * kmPrice;
+
+    // Age request
+
+    let ageInput = prompt('Inserisci la tua età');
+    let age = Number(ageInput);
+    
+    // Verification if prompt input is a number or not
+
+    if (!isNaN(age)) {
+        if (age >= 0 && age <= 18 ) {
+            priceUnder18 = price - (price * discount20);
+            console.log(priceUnder18);
+            
+        }
+        else if (age >= 65) {
+            priceOver65 = price - (price * discount40);
+            console.log(priceOver65);
+            
+        } else {
+            console.log(price);
+            
+        }
+    } else {
+        alert("Per favore aggiorna la pagina ed inserisci un numero nel campo dell'età");
+    }
 } else {
     alert('Per favore aggiorna la pagina ed inserisci un numero');
-    
 }
