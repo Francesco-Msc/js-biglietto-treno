7977824eed032c1c6cf8344bb15ff2ec308d9d97
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-10-08 02:28:45 +0200

    reorganized js file

--- javascript/main.js
+++ javascript/main.js
@@ -1,49 +1,40 @@
-// Price per km
+//info request
+
+let distanceInput = prompt('inserisci la distanza da percorrere');
+let ageInput = prompt("inserisci l'età del passegero");
+
+//conversion into numbers
+
+let km = Number(distanceInput);
+let age = Number(ageInput);
+
+//asigned variables
+
 const kmPrice = 0.21;
-
-// Distance request
-let distance = prompt('Inserisci i km che vuoi percorrere');
-
-// Distance conversion into number
-let km = Number(distance);
-
-//Asigned viriables
-
 let price;
-let priceUnder18;
-let priceOver65;
+
+if (!isNaN(km) && !isNaN(age) && (km >= 0 && age >= 0)){             //verification if inputs are numbers
+    price = km * kmPrice;                                            //full price
+} 
+else {
+    alert ('inserisci dei numeri validi nei campi richiesti');
+}
+
+//discounts
+
 const discount20 = 0.2;
 const discount40 = 0.4;
+let priceUnder18;
+let priceOver65;
 
-// Verification if prompt input is a number or not
-
-if (!isNaN(km) && km >= 0) {
-    price = km * kmPrice;
-
-    // Age request
-
-    let ageInput = prompt('Inserisci la tua età');
-    let age = Number(ageInput);
-    
-    // Verification if prompt input is a number or not
-
-    if (!isNaN(age) && age >= 0) {
-        if (age <= 18 ) {
-            priceUnder18 = price - (price * discount20);
-            document.getElementById('yourPrice').innerHTML = `€ ${priceUnder18.toFixed(2)}`;
-            
-        }
-        else if (age >= 65) {
-            priceOver65 = price - (price * discount40);
-            document.getElementById('yourPrice').innerHTML = `€ ${priceOver65.toFixed(2)}`;
-            
-        } else {
-            document.getElementById('yourPrice').innerHTML = `€ ${price.toFixed(2)}`;
-            
-        }
-    } else {
-        alert("Per favore aggiorna la pagina ed inserisci un numero valido nel campo dell'età");
-    }
-} else {
-    alert('Per favore aggiorna la pagina ed inserisci un numero valido');
+if (age < 18){                                                                               //price 20% off for under 18
+    priceUnder18 = price - (price * discount20);
+    document.getElementById('yourPrice').innerHTML = `€ ${priceUnder18.toFixed(2)}`;
+} 
+else if (age > 65){                                                                         //price 40% off for over 65
+    priceOver65 = price - (price * discount40);
+    document.getElementById('yourPrice').innerHTML = `€ ${priceOver65.toFixed(2)}`;
+}
+else{
+    document.getElementById('yourPrice').innerHTML = `€ ${price.toFixed(2)}`;
 }
